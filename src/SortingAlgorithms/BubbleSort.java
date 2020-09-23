383c5f2048dae68ee147dd70ad033492d116e55b
John Hunter <jhhunter@buffalo.edu>
2020-09-23 16:45:37 -0400

    Implemented insertion sort and gave descriptions of bubble and insertion sort. Added the average runtime of the algorithms as well.

--- src/SortingAlgorithms/BubbleSort.java
+++ src/SortingAlgorithms/BubbleSort.java
@@ -2,6 +2,18 @@ package SortingAlgorithms;
 
 public class BubbleSort extends Sort {
 
+    /**
+     * Implementation of Bubble Sort
+     *
+     * Average Performance: O(n^2)
+     *
+     * Works by iterating through the array, comparing each element to the next
+     * and swapping them if the first integer is smaller than the next. It keeps
+     * iterating through the list until nothing is swapped meaning the items are
+     * all sorted
+     * @param unsorted_list - An unsorted array of integers
+     * @return a sorted array of integers
+     */
     @Override
     public int[] sort(final int[] unsorted_list) {
         int n = unsorted_list.length;
--- src/SortingAlgorithms/InsertionSort.java
+++ src/SortingAlgorithms/InsertionSort.java
@@ -2,8 +2,30 @@ package SortingAlgorithms;
 
 public class InsertionSort extends Sort {
 
+    /**
+     * Implementation of insertion sort.
+     *
+     * Average Performance: O(n^2)
+     *
+     * Works by going to each index starting at 1 and comparing the value at the index
+     * to the to the values before it. While it is less than the value at an index before
+     * it, it continues to compare to smaller indexes until it is larger. This way, items
+     * are put into the correct spot as they are sorted.
+     * @param unsorted_list - The unsorted array
+     * @return a sorted array of integers
+     */
     @Override
-    public int[] sort(int[] unsorted_list) {
-        return super.sort(unsorted_list);
+    public int[] sort(final int[] unsorted_list) {
+        int[] sorted_list = unsorted_list;
+        for(int i = 1; i < unsorted_list.length; i++) {
+            int sort_val = sorted_list[i];
+            int j = i -1;
+            while(j >= 0 && sorted_list[j] > sort_val) {
+                sorted_list[j + 1] = sorted_list[j];
+                j-=1;
+            }
+            sorted_list[j+1] = sort_val;
+        }
+        return sorted_list;
     }
 }
